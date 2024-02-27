379dc7a788c3962d190ce25913fb31855af14a22
Guilherme Rodrigues <guilhermelindinho2@hotmail.com>
2024-02-27 20:07:13 -0300

    new api for menu food

--- src/main/java/com/guilhermerodrigues/menu/food/FoodRepository.java
+++ src/main/java/com/guilhermerodrigues/menu/food/FoodRepository.java
@@ -3,8 +3,6 @@ package com.guilhermerodrigues.menu.food;
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface FoodRepository extends JpaRepository<Food, Long> {
 
     @Transactional
